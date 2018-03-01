35d22e213e042270194d210ac434375b758578be
Kelby Chen <kchen13@outlook.com>
2018-03-01 02:07:38 -0500

    Fixed bug if user has no weapons and tries to attack. Fixed armor calculations, created best_armor Fixed health regen to use health amount, created health_list Added ParkingLot, MysteriousRoom Added Weapon Fists, doesn't take up inventory Added Sound Fist, Health Player starts with no items Starting Room renamed to Hospital Lobby Tweaked map

--- mod_sound_effects.py
+++ mod_sound_effects.py
@@ -7,26 +7,21 @@ from pygame import mixer
 path = os.path.dirname(__file__)
 mixer.init(channels=3)
 
-starting_room = path + '/sound effects/00-StartingRoom.ogg'
-# Credit: https://freesound.org/people/RokZRooM/sounds/344778/
+# Sound credits @ EOF
+background_sound = path + '/sound effects/00-StartingRoom.ogg'
 cold_room = mixer.Sound(path + '/sound effects/01-ColdRoom.ogg')
-# Credit: https://freesound.org/people/Kastenfrosch/sounds/162486/
 supply_room = mixer.Sound(path + '/sound effects/02-SupplyRoom.ogg')
-# Credit: https://freesound.org/people/Legato87/sounds/72741/
 typing_sound = mixer.Sound(path + '/sound effects/SE-Typing.ogg')
-# Credit: https://freesound.org/people/Natty23/sounds/257275/
 ghoul01_sound = mixer.Sound(path + '/sound effects/SE-Ghoul01.ogg')
-# Credit: https://freesound.org/people/limetoe/sounds/317017/
 knife_sound = mixer.Sound(path + '/sound effects/SE-Knife.ogg')
-# Credit: https://freesound.org/people/JoelAudio/sounds/77611/
 enemy_dead = mixer.Sound(path + '/sound effects/SE-EnemyDead.ogg')
+fist_sound = mixer.Sound(path + '/sound effects/SE-Fists.ogg')
+health_sound = mixer.Sound(path + '/sound effects/SE-Health.ogg')
 
-
-# Credit: https://freesound.org/people/limetoe/sounds/249524/
 
 def background():
     mixer.music.set_volume(0.4)
-    mixer.music.load(starting_room)
+    mixer.music.load(background_sound)
     mixer.music.play(-1)
 
 
@@ -51,5 +46,24 @@ def knife():
     mixer.Sound.play(knife_sound)
 
 
+def fists():
+    mixer.Sound.play(fist_sound)
+
+
+def health():
+    mixer.Sound.play(health_sound)
+
+
 def killed_enemy():
     mixer.Sound.play(enemy_dead)
+
+# Credits
+# background: https://freesound.org/people/RokZRooM/sounds/344778/
+# cold: https://freesound.org/people/Kastenfrosch/sounds/162486/
+# supply: https://freesound.org/people/Legato87/sounds/72741/
+# typing: https://freesound.org/people/Natty23/sounds/257275/
+# ghoul01: https://freesound.org/people/limetoe/sounds/317017/
+# knife: https://freesound.org/people/JoelAudio/sounds/77611/
+# killed enemy: https://freesound.org/people/limetoe/sounds/249524/
+# fists: https://freesound.org/people/josepharaoh99/sounds/361636/
+# health: https://freesound.org/people/GameAudio/sounds/220173/
--- tiles.py
+++ tiles.py
@@ -52,10 +52,12 @@ class EnemyRoom(MapTile):
         super().__init__(x, y)
 
     def modify_player(self, the_player):
+        the_player.best_armor()
         if self.enemy.is_alive():
-            the_player.hp = the_player.hp - self.enemy.damage
-            print('The {} does {} damage.\nYou have {} HP remaining.'.
-                  format(self.enemy.name, self.enemy.damage, the_player.hp))
+            damage = self.enemy.damage - (the_player.armor * 0.1)
+            the_player.hp = the_player.hp - (self.enemy.damage - the_player.armor * 0.1)
+            print('{} does {} damage.\nYou have {} HP remaining.'.
+                  format(self.enemy.name, damage, the_player.hp))
 
     def available_actions(self):
         if self.enemy.is_alive():
@@ -66,7 +68,7 @@ class EnemyRoom(MapTile):
             return MapTile.available_actions(self)
 
 
-class StartingRoom(MapTile):
+class HospitalLobby(MapTile):
     room_id = 'Hospital Lobby'
     room_inventory = []
 
@@ -86,7 +88,7 @@ class StartingRoom(MapTile):
                                     'electricity, you have to abandon him.\n'
                                     '\nThe dropping temperature is causing you to look for supplies and search for a\n'
                                     'warm safe environment. You leave Frank’s room and have a few options where to\n'
-                                    'look for supplies.\n')
+                                    'look for supplies.\nYou stand in the hospital lobby awaiting your fate. \n')
         else:
             print('Doc, what are you smoking? You started here. Get moving!')
         return """"""
@@ -94,6 +96,26 @@ class StartingRoom(MapTile):
     def modify_player(self, player):
         # Room has no action on player
         pass
+
+
+class MysteriousRoom(MapTile):
+    room_id = 'oddly mysterious room is this direction.'
+    room_inventory = [items.DoctorsCoat(), items.Knife()]
+
+    def intro_text(self):
+        coordinates = str(self.x) + str(self.y)
+        if mod_movement_history.check_history(coordinates):
+            mod_slow_text.slow_text("There's a mist in the air, could be somethings in that cabinet though.\n")
+        else:
+            print('Not so mysterious anymore.')
+        return """"""
+
+    @staticmethod
+    def modify_player(player):
+        damage = 4 - (player.armor * 0.1)
+        player.hp -= damage
+        print('Your body temperature is dropping.\nYou lost {} health.\nYour HP is currently: {}\n'
+              .format(damage, player.hp))
 
 
 class ColdRoom(MapTile):
@@ -104,35 +126,37 @@ class ColdRoom(MapTile):
         coordinates = str(self.x) + str(self.y)
         mod_sound_effects.cold()
         if mod_movement_history.check_history(coordinates):
-            mod_slow_text.slow_text("\nA cold fierce wind hits your body.")
+            mod_slow_text.slow_text("\nA cold fierce wind hits your body.\n")
         else:
-            mod_slow_text.slow_text("\nA cold fierce wind reminds you that you've been here before.")
+            mod_slow_text.slow_text("\nA cold fierce wind reminds you that you've been here before.\n")
         return """"""
 
     @staticmethod
     def modify_player(player):
-        # Armor HP percentage subtracted from damage taken
-        player.hp -= 5 - (player.armor * 0.1)
-        print('You lost 5 health. Your HP is currently:', player.hp, '\n')
+        damage = 8 - (player.armor * 0.1)
+        player.hp -= damage
+        print('You lost {} health.\nYour HP is currently: {}\n'.format(damage, player.hp))
 
 
 class SupplyRoom01(MapTile):
     room_id = 'looks like there could be supplies in this room.'
-    room_inventory = [items.DoctorsCoat()]
+    room_inventory = [items.FirstAid(), items.Bandages()]
 
     def intro_text(self):
         coordinates = str(self.x) + str(self.y)
         mod_sound_effects.supply()
         if mod_movement_history.check_history(coordinates):
-            mod_slow_text.slow_text("\nIt's some sort of supplies room. Searching may be of your best interest.")
+            mod_slow_text.slow_text("\nIt's some sort of supplies room. Searching may be of your best interest.\n")
         else:
-            mod_slow_text.slow_text("\nThis is that supply room you were in earlier.")
+            mod_slow_text.slow_text("\nThis is that supply room you were in earlier.\n")
         return """"""
 
     @staticmethod
     def modify_player(player):
-        player.hp -= 2 - (player.armor * 0.1)
-        print('Your body temperature is dropping. You lost 2 health. Your HP is currently:', player.hp, '\n')
+        damage = 4 - (player.armor * 0.1)
+        player.hp -= damage
+        print('Your body temperature is dropping.\nYou lost {} health.\nYour HP is currently: {}\n'
+              .format(damage, player.hp))
 
 
 class GhoulRoom01(EnemyRoom):
@@ -145,11 +169,34 @@ class GhoulRoom01(EnemyRoom):
     def intro_text(self):
         if self.enemy.is_alive():
             mod_sound_effects.ghoul01()
-            mod_slow_text.slow_text("\nHoly crap, it's a God damn Ghoul!"
+            mod_slow_text.slow_text("\nHoly crap, it's a God damn ghoul!"
                                     "\nGhoul HP:" + str(self.enemy.hp))
         else:
             mod_slow_text.slow_text("\nHa! That ghoul looks like Tom Brady after Super Bowl LII.")
         return """"""
+
+
+class ParkingLot(MapTile):
+    room_id = 'Parking Lot of the Hospital'
+    room_inventory = [items.FirstAid(), items.Bandages()]
+
+    def intro_text(self):
+        coordinates = str(self.x) + str(self.y)
+        mod_sound_effects.supply()
+        if mod_movement_history.check_history(coordinates):
+            mod_slow_text.slow_text("\nThe moment you walk out you realize how quickly the temperature has fallen. In "
+                                    "order to survive you're going to need to find warmth and shelter. Better decide "
+                                    "quick before you die from the frigid coldness.\n")
+        else:
+            mod_slow_text.slow_text("\nIt's blistering cold and there's a sign that says No Loitering.")
+        return """"""
+
+    @staticmethod
+    def modify_player(player):
+        damage = 8 - (player.armor * 0.1)
+        player.hp -= damage
+        print('Your body temperature is dropping.\nYou lost {} health.\nYour HP is currently: {}\n'
+              .format(damage, player.hp))
 
 
 class WinningRoom(MapTile):
